da37c72eab0dfee14c563f040d00a4d3c5811803
Marton Lederer <marton@lederer.hu>
2024-11-25 16:52:40 +0100

    feat: finish main ui elements

--- src/hu/martonlederer/hotel/ui/MainFrame.java
+++ src/hu/martonlederer/hotel/ui/MainFrame.java
@@ -1,19 +1,30 @@
 package hu.martonlederer.hotel.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JComponent;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 public class MainFrame extends JFrame {
 	private CalendarData data;
@@ -49,13 +60,48 @@ public class MainFrame extends JFrame {
         // naptár
         JTable calendar = new JTable(data);
         JScrollPane scrollPane = new JScrollPane(calendar);
+        
         calendar.setFillsViewportHeight(true);
         calendar.getTableHeader().setReorderingAllowed(false);
+        calendar.setRowHeight(100);
+        calendar.setCellSelectionEnabled(true);
+        calendar.setRowSelectionAllowed(false);
+        calendar.setColumnSelectionAllowed(false);
+        calendar.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+                component.setFont(new Font("Arial", Font.BOLD, 18));
+                ((JComponent) component).setBorder(new LineBorder(Color.WHITE, 10));
+
+                setHorizontalAlignment(SwingConstants.RIGHT);
+                setVerticalAlignment(SwingConstants.BOTTOM);
+
+                //setBackground(new Color(255, 255, 204));
+
+                return component;
+            }
+        });
+        calendar.addMouseListener(new MouseAdapter() {
+        	@Override
+            public void mouseClicked(MouseEvent e) {
+                int row = calendar.rowAtPoint(e.getPoint());
+                int column = calendar.columnAtPoint(e.getPoint());
+
+                if (row >= 0 && column >= 0) {
+                    Object cellValue = calendar.getValueAt(row, column);
+                    System.out.println(cellValue);
+                }
+            }
+        });
+                        
         add(scrollPane, BorderLayout.CENTER);
 
         // navigáció
         JPanel nav = new JPanel();
         
+        JButton editHotelBtn = new JButton("Edit hotel");
         dateLabel = new JLabel(data.getStartDate().toString());
         JButton prev = new JButton("<");
         JButton next = new JButton(">");
@@ -63,6 +109,7 @@ public class MainFrame extends JFrame {
         prev.addActionListener(new DateNavBtnListener(-1));
         next.addActionListener(new DateNavBtnListener(1));
         
+        nav.add(editHotelBtn);
         nav.add(prev);
         nav.add(dateLabel);
         nav.add(next);
